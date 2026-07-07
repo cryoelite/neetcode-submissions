fa15f71e60348c3d65f4a396c9b6bce5b1e9484f
Arvind Sagar <itscryonim@gmail.com>
2026-07-07 09:01:24 +0530

    Add: best-time-to-buy-and-sell-stock-ii - submission-1

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxP{0};
+        int totalP{0};
+        auto l{prices.begin()};
+        auto r{prices.begin()+1};
+
+        while(r<prices.end()){
+            if(*l<*r && *r>=*(r-1)){
+                int profit{*r-*l};
+                totalP+=max(profit,maxP) - maxP;
+                maxP= max(profit, maxP);
+                
+            } else if (*r<*(r-1)){
+                l=r;
+                 //cleared previous shares and now ready to track again
+                maxP=0;
+            } else {
+                l=r;
+            }
+            ++r;
+        }
+        return totalP;
+    }
+};
