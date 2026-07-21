8f94c46277b64ef01ade31a41f92e3324486cc50
Arvind Sagar <itscryonim@gmail.com>
2026-07-21 19:00:44 +0530

    Add: contains-duplicate-ii - submission-3

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-3.cpp
@@ -0,0 +1,24 @@
+class Solution {
+   public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        if (k == 0) {
+            return false;
+        }
+        auto l{nums.begin()};
+        auto r{nums.begin() + 1};
+        while (r < nums.end()) {
+            cout << "Checking l: " << *l << " r: " << *r << '\n';
+            if (l != r && *l == *r && distance(l, r) <= k) {
+                return true;
+            }
+
+            if (distance(l, r + 1) > k) {
+                ++l;
+            } else {
+                ++r;
+            }
+        }
+
+        return false;
+    }
+};
