b05a25e50dad382406e4d45b81fccb7955319a1e
Arvind Sagar <itscryonim@gmail.com>
2026-07-28 19:44:58 +0530

    Add: baseball-game - submission-1

---
+++ Data Structures & Algorithms/baseball-game/submission-1.cpp
@@ -0,0 +1,37 @@
+#include <stack>
+class Solution {
+   public:
+    int calPoints(vector<string>& operations) {
+        stack<int> s{};
+        int res{0};
+        for (const string& op : operations) {
+            // We parse the string to int at the very end (to avoid error handling with stoi
+            // function) We can perform the rest of the operations before that in the if/else
+            // branches, because the question's constraints guarantee that there will be elements in
+            // the record before any operation is requested. If this guarantee was not there, we
+            // would have to change the logic a bit.
+            if (op == "+") {
+                int top{s.top()};
+                s.pop();  // Have to remove the top element to access the one before
+                int sum{top + s.top()};
+                s.push(top);
+                s.push(sum);
+                res += sum;
+            } else if (op == "C") {
+                res -= s.top();
+                s.pop();
+            } else if (op == "D") {
+                res += (s.top() * 2);
+                s.push(s.top() * 2);
+            } else {
+                // String is an int as that's the only input left
+                int iop{stoi(op)};
+                s.push(iop);
+
+                res += iop;
+            }
+        }
+
+        return res;
+    }
+};
