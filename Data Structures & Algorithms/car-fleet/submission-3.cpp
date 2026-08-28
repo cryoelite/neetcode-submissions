0de6e085fae0fb571bd1cbb1fa53a3bf5b903ac3
Arvind Sagar <itscryonim@gmail.com>
2026-08-28 17:24:39 +0530

    Add: car-fleet - submission-3

---
+++ Data Structures & Algorithms/car-fleet/submission-3.cpp
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <ranges>
+struct Fleet {
+    int pos;
+    int speed;
+    Fleet(int pos, int speed) : pos(pos), speed(speed) {}
+    Fleet() = delete;
+};
+class Solution {
+   public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        vector<Fleet> fleets{};
+        vector<double> res{};  // time, stack
+        int fleet_count{0};
+        const int n = position.size();
+        fleets.reserve(n);
+
+        for (int i{0}; i < n; ++i) {
+            fleets.push_back(Fleet(position[i], speed[i]));
+        }
+        sort(fleets.begin(), fleets.end(),
+             [](const Fleet& a, const Fleet& b) { return a.pos > b.pos; });
+        // sorted in descending order, or rather monotonically non-increasing order
+
+        for (const auto& elem : fleets) {
+            double time = static_cast<double>((target - elem.pos)) / (elem.speed);
+            cout << time << '\n';
+            if (!res.empty() && res.back() >= time) {
+                //Nothing to do
+            } else {
+                res.push_back(time);
+                ++fleet_count;
+            }
+        }
+
+        return fleet_count;
+    }
+};
